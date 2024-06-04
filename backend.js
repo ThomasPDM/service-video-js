a291beb4845d441dc9d65cf5a678abb9c21bd341
ThomasPDM <thomas.pelissie@laposte.net>
2024-06-04 15:35:36 +0200

    correct backend.js

--- backend.js
+++ backend.js
@@ -8,40 +8,39 @@ const fs = require('fs'); // pour lire des fichiers
 const app = express(); 
 
 
-
-
-// DÉBUT DES MODIFICATIONS
-
 // Récupération de la vidéo via la route /video
-app.get('/videos/:nom', (req, res) => {
-    // :nom et :code sont des paramètres de la route
-    console.log(req.params.nom);
+app.get('/videos/:nom/:code', (req, res) => {
+        // :nom et :code sont des paramètres de la route
+        // console.log(req.params.nom);
+        // console.log(req.params.code);
 
-    // Création d'un objet URL pour le fichier videos.json
-    const url = new URL(/*TODO*/); 
+        // Création d'un objet URL pour le fichier videos.json
+        const url = new URL('file:'+path.join(__dirname, 'videos.json')); 
 
-    // Récupération du fichier videos.json
-    const les_videos = JSON.parse(fs.readFileSync(/*TODO*/));
+        // Récupération du fichier videos.json
+        const les_videos = JSON.parse(fs.readFileSync(url));
 
-    // Création d'une variable permettant de savoir si il y a ou non correspondance
-    /*TODO*/
+        // Création d'une variable permettant de savoir si il y a ou non correspondance
+        let good = false; 
 
-    // Parcours de la liste des vidéos pour chercher une correspondance
-    for(i=0; i<les_videos.length; i++)
-    {
-        /*TODO*/
+        // Parcours de la liste des vidéos pour chercher une correspondance
+        for(i=0; i<les_videos.length; i++)
+        {
+            if(les_videos[i].nom == req.params.nom && les_videos[i].code == req.params.code) // Si le nom correspond
+            {        
+                good = true; // Indique que l'accès est autorisé
+            }
+        }
+
+        // Si l'accès est autorisé, on renvoie la vidéo
+        if (good) {
+            res.sendFile(path.join(__dirname, 'videos', req.params.nom+'.mp4'));
+        }
+        else {
+            res.send("rien ne correspond");
+        }
     }
-
-    // Si le nom correspond, on renvoie la vidéo (res.sendFile)
-    /*TODO*/
-}
 );
-
-
-// FIN DES MODIFICATIONS
-
-
-
 
 
 // Rend accessible depuis localhost les fichiers du dossier public
